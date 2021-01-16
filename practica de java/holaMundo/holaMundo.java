347f691a6911b5ed3ba1cf4255906b3c7fd8d807
irvyn2703 <tosolatoso17@gmail.com>
2021-01-16 14:53:14 -0600

    ahora es progra

---
+++ practica de java/holaMundo/holaMundo.java
@@ -0,0 +1,9 @@
+public class holaMundo {
+    private String saludo;
+        public holaMundo(){
+            saludo="hola, mundo";
+        }
+        public void mostrar(){
+            System.out.println("saludo: " + saludo);
+        }
+}
